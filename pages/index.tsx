0330751dd77a1808c8877583c2eb38366b8552cc
Taras.Kravets <taraskravets95@gmail.com>
2023-03-25 14:45:26 +0200

    feat(UI): implemented UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,10 @@ import Head from 'next/head';
 import { Poppins } from 'next/font/google';
 
 import Hero from '@/components/Hero';
+import Shorten from '@/components/Shorten';
+import Stats from '@/components/Stats';
+import Feature from '@/components/Feature';
+import Footer from '@/components/Footer';
 
 const poppins = Poppins({
 	subsets: ['latin'],
@@ -16,6 +20,10 @@ export default function Home() {
 			</Head>
 			<main className={poppins.className}>
 				<Hero />
+				<Shorten />
+				<Stats />
+				<Feature />
+				<Footer />
 			</main>
 		</>
 	);
